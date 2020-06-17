3ec6b3b111d819a5fd81b4a65f4fdd832de86099
Kartik <aggarwalkartik96@gmail.com>
2020-06-17 13:00:51 +0530

    Updates

---
+++ precache-manifest.2fc8f78617c2e6b9e5f937f2e1269b11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b22e508b204fadc42e985009d329ef9",
+    "url": "/videoplayer/index.html"
+  },
+  {
+    "revision": "446511ec1960dfb9dd00",
+    "url": "/videoplayer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "22e031d65e60bf2e204c",
+    "url": "/videoplayer/static/css/main.fd529781.chunk.css"
+  },
+  {
+    "revision": "446511ec1960dfb9dd00",
+    "url": "/videoplayer/static/js/2.5edfa5b5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/videoplayer/static/js/2.5edfa5b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e031d65e60bf2e204c",
+    "url": "/videoplayer/static/js/main.d0c2c922.chunk.js"
+  },
+  {
+    "revision": "4d1c31e377a259862913",
+    "url": "/videoplayer/static/js/runtime-main.d7536566.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/videoplayer/static/media/logo.5d5d9eef.svg"
+  }
+]);
